1e4598b952b9fa5dfe083670d4a4f17e163c6ac1
Christian Janker <janker.christ@gmail.com>
2019-08-11 21:12:47 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+declare const VERSION: string;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-with-tailwind';
+  title = 'angular-with-tailwind: ' + VERSION;
 }
---
+++ webpack.partial.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [
+                  require("postcss-import"),
+                  require('tailwindcss'),
+                  require('autoprefixer')
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "VERSION": JSON.stringify("4711")
+        })
+    ]
+}
