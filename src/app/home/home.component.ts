7e3ba856c4d52051b9ae686d21d3e4f8c38758a5
Christian Janker <janker.christ@gmail.com>
2019-12-28 14:36:21 +0100

    WIP

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,13 +5,21 @@ import {posts} from './posts-config';
   selector: 'blog-home',
   template: `
     <blog-text-container>
-        <h1>Hi there! </h1>
+        <div class="md:flex justify-center">
+          <h1>Hi there</h1>
+          <img class="avatar" src="/assets/me_u_small2.jpg">
+          <h1>I'm Christian</h1>
 
+        </div>
+        <div class="md:flex justify-center mb-10">
+          <p>I like to write on software topics!</p>
+          <a href="https://twitter.com/y_a_n_x" target="_blank" class="self-center"><img class="h-6 w-6 mx-3" src="/assets/twitter.svg"></a>
+        </div>
         <div *ngFor="let post of posts">
-
-          <blog-text><b><a href="#" [routerLink]="post.route">{{ post.title }}</a></b></blog-text>
-          <blog-list-date>{{ post.publishedAt }}</blog-list-date>
-
+          <div class="post-header">
+            <blog-text><b><a href="#" [routerLink]="post.route">{{ post.title }}</a></b></blog-text>
+            <blog-list-date class="">{{ post.publishedAt }}</blog-list-date>
+          </div>
           <blog-text>{{ post.summary }}</blog-text>
           <hr>
         </div>
--- src/app/home/posts-config.ts
+++ src/app/home/posts-config.ts
@@ -2,15 +2,10 @@ export const posts = [
   {
     id: 1,
     title: 'Don\'t fear the boilerplate - Fear tight coupling!',
-    summary: 'Many fear state. But be with me an conquer the boilerplate!',
+    summary: `Many developers rant about the boilerplate that is produced when introducing a redux 
+      based state management library in their Angular application. I can understand those developers
+      because every abstraction has its price and state management can become hairy really fast.`,
     publishedAt: '2018-12-12',
     route: 'angular-state-management'
-  },
-  {
-    id: 2,
-    title: 'Having fun with Mixins in Angular',
-    summary: 'Explore the world of Mixins. Learn them. See how Angular Material is using them',
-    publishedAt: '2018-11-12',
-    route: 'reusable-containers'
   }
 ];
